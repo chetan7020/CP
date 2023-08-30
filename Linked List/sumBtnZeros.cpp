6138c78a6dde5707786b6b1ba9b5851d74877bd8
TMR <chetandagajipatil333@gmail.com>
2023-08-30 19:37:05 +0530

    sum betn zeros

---
+++ Linked List/sumBtnZeros.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    ListNode* mergeNodes(ListNode* head) {
+        while(head!=NULL && head->val!=0) head=head->next;
+        
+        ListNode* ans= new ListNode(-1);
+        ListNode* temp=ans;
+        
+        int sum=0;
+        
+        while(head!=NULL){
+            sum+=head->val;
+            if(head->val==0 && sum!=0){
+                temp->next=new ListNode(sum);
+                temp=temp->next;
+                sum=0;
+            }
+            
+            head=head->next;
+        }
+        
+        return ans->next;
+    }
+};
